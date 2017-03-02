3f4baef6c8c24d3cc43378ba1a52f15e67f410ea
Sean McMurray <sean@mvdsl.com>
2017-03-02 08:01:51 -0800

    Cleaned up path handling, appending mw, etc.

--- index.js
+++ index.js
@@ -17,9 +17,11 @@
         this.path = null
       }
       else {
-        this.keys = []
-        this.path = p2re(path, this.keys, options)
         this.routers = null
+        if (path){
+          this.keys = [];
+          this.path = p2re(path, this.keys, options)
+        }
       }
       this.mw = []
 
@@ -29,21 +31,21 @@
     }
 
     use(path, ...mw){
-      let chain = 'function' != typeof path
+      let chain = 'function' === typeof path
         ? [].concat(path, mw)
         : [].concat(path).map(p=>new Router(p).use(...mw).routes())
 
       if (this.routers){
-        this.routers.forEach(r=>r.use(...mw))
+        this.routers.forEach(r=>r.use(chain))
       }
-      else this.mw.push(...mw)
+      else this.mw = this.mw.concat(chain)
       return this
     }
 
     on(methods, path, ...mw){
       let chain;
       if ('function' != typeof path){
-        return mw.length ? this.use(new Router(p).on(methods,...mw).routes()) : this
+        return mw.length ? this.use(new Router(path).on(methods,...mw).routes()) : this
       }
       if (!path){
         console.error('Missing middleware argument for route method')
@@ -51,7 +53,7 @@
       }
       chain = ignore(path, ...mw).unless(ctx=>{
         let M = ctx.method.toUpperCase()
-        let Methods = methods && methods.map(m=>m.toUpperCase());
+        let Methods = methods && [].concat(methods).map(m=>m.toUpperCase());
         return !methods || ~Methods.indexOf(M) || (M==='HEAD' && ~Methods.indexOf('GET'))
       });
 
@@ -69,14 +71,14 @@
             return next()
           }
           ctx.routed = (ctx.routed||'')+match.shift()
-          ctx.params = this.match.reduce((p,k,i)=>{
+          ctx.params = match.reduce((p,k,i)=>{
             let v = k ? decodeURIComponent(k) : null
             p[i] = v
             if (this.keys.length) p[this.keys[i].name] = v
             return p
           }, ctx.params || {})
         }
-        return compose([].concat(this.routers ? this.routers.map(r=>r.routes()) : this.mw))(ctx,next)
+        return compose(this.routers ? this.routers.map(r=>r.routes()) : this.mw)(ctx,next)
       }
     }
   }
